21ebc7340d04ab38241e9939a466f2a1fcb16f9b
Erian <e_seagull-01@hotmail.com>
2022-08-18 13:11:47 -0500

    buttons and select menus

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,18 +1,14 @@
-import { CommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction } from "discord.js";
 import { ClientBot } from "index";
 export default {
     name: "interactionCreate", 
     once: false,
-    async execute(interaction : CommandInteraction) {
+    async execute(interaction : ChatInputCommandInteraction) {
         const { client } : { client : ClientBot } = interaction;
         if (!interaction.isChatInputCommand()) {
             return;
         }
 
-        if (!interaction.isButton()) {
-            return;
-        }
-    
         const command = client.commands?.get(interaction.commandName);
     
         if (!command) {
